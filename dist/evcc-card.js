3787eca788d4f1a229629542fa0cb374382411c5
bw <bastian@familie-will.org>
2026-06-23 18:57:07 +0200

    Fix: version bump

--- dist/evcc-card.js
+++ dist/evcc-card.js
@@ -8,7 +8,7 @@
  *                /config/www/evcc-card/locales/en.json
  */
 
-const EVCC_CARD_VERSION = "0.6.2";
+const EVCC_CARD_VERSION = "0.6.3";
 
 const FEATURES = [
   { suffix: "mode",                domain: "select",        type: "mode",          lp: true,  core: true },
