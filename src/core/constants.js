6e06b58ff688b965d9e23261d8c3e2aed31f6819
bw <bastian@familie-will.org>
2026-09-22 11:58:11 +0200

    0.8.3: bump version

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,4 +1,4 @@
-export const EVCC_CARD_VERSION = "0.8.2";
+export const EVCC_CARD_VERSION = "0.8.3";
 
 export const FEATURES = [
   { suffix: "mode",                domain: "select",        type: "mode",          lp: true,  core: true },
